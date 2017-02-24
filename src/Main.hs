d732e990aae86a7ef9e718574a8836f585a5cde9
Mantas Gudynas <m.gudynas@tracsis.com>
2017-02-24 11:10:08 +0000

    Initial working example

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,31 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
 module Main where
 
+import           Control.Monad.IO.Class  (liftIO)
+import           Database.Persist
+import           Database.Persist.Sqlite
+import           Database.Persist.TH
+
+share [mkPersist sqlSettings, mkSave "entityDefs"] [persistLowerCase|
+Person
+    name String
+    age Int
+    deriving Show
+|]
+
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = runSqlite ":memory:" $ do
+    runMigration $ migrate entityDefs $ entityDef (Nothing :: Maybe Person)
+    michaelId <- insert $ Person "Michael" 26
+    michael <- get michaelId
+    liftIO $ print michael
